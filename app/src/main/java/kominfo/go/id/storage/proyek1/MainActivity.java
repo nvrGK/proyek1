f416839c22c57a41f7e91c6cdf7ec44db20279e8
DERI <gerrykurniawan98@gmail.com>
2021-10-25 23:44:36 +0700

    Initial commit

---
+++ app/src/main/java/kominfo/go/id/storage/proyek1/MainActivity.java
@@ -0,0 +1,37 @@
+package kominfo.go.id.storage.proyek1;
+
+import android.Manifest;
+import android.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.Enviroment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.appwidget.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+public static final int REQUEST_CODE_STORAGE = 100;
+ListView listView;
+
+
+
+}
---
+++ app/src/main/java/kominfo/go/id/storage/proyek1/SplashScreen.java
@@ -0,0 +1,23 @@
+package kominfo.go.id.storage.proyek1;
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, SplashScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
